ccfc963ee75062cb387eed9153174b58a5e21293
Jonathan Augustin <tun45218@temple.edu>
2024-03-21 11:24:38 -0400

    Added constructors in ViewModel

--- app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
+++ app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
@@ -1,7 +1,17 @@
 package edu.temple.diceroll
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DiceViewModel : ViewModel() {
-    
+    private val selectedNumber = MutableLiveData<Int>()
+
+    fun setSelectedNumber(side: Int) {
+        selectedNumber.value = side
+    }
+
+    fun getSelectedNumber(): LiveData<Int> {
+        return selectedNumber
+    }
 }
